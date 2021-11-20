dcea6379a24b83c5cad9854b683a718478f7ab1b
Xname <xnameserver@o2.pl>
2021-11-20 19:16:19 +0100

    i guess finished

---
+++ CustomMTF/Items/GuardCommanderKeycardItem.cs
@@ -0,0 +1,113 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="GuardCommanderKeycardItem.cs" company="Mistaken">
+// Copyright (c) Mistaken. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using Exiled.API.Features.Spawn;
+using Exiled.Events.EventArgs;
+using MEC;
+using Mistaken.API.CustomItems;
+using Mistaken.API.CustomRoles;
+using Mistaken.API.Extensions;
+using Mistaken.API.GUI;
+using Mistaken.RoundLogger;
+using UnityEngine;
+
+namespace Mistaken.CustomMTF.Items
+{
+    /// <summary>
+    /// Keycard that Guard commander uses.
+    /// </summary>
+    public class GuardCommanderKeycardItem : MistakenCustomItem
+    {
+        /// <summary>
+        /// Gets or sets owner of the item.
+        /// </summary>
+        public Player CurrentOwner { get; set; }
+
+        /// <inheritdoc/>
+        public override MistakenCustomItems CustomItem => MistakenCustomItems.GUARD_COMMANDER_KEYCARD;
+
+        /// <inheritdoc/>
+        public override ItemType Type { get; set; } = ItemType.KeycardNTFOfficer;
+
+        /// <inheritdoc/>
+        public override string Name { get; set; } = "Karta Dowódcy Ochrony";
+
+        /// <inheritdoc/>
+        public override string Description { get; set; } = "Guard Commander's Keycard";
+
+        /// <inheritdoc/>
+        public override float Weight { get; set; } = 0.01f;
+
+        /// <inheritdoc/>
+        public override SpawnProperties SpawnProperties { get; set; }
+
+        /// <inheritdoc/>
+        public override Pickup Spawn(Vector3 position)
+        {
+            Item keycard = new Item(this.Type);
+            RLogger.Log("GUARD COMMANDER KEYCARD", "SPAWN", $"{this.Name} spawned");
+            return this.Spawn(position, keycard);
+        }
+
+        /// <inheritdoc/>
+        public override Pickup Spawn(Vector3 position, Item item)
+        {
+            item.Scale = Handlers.GuardCommanderHandler.Size;
+            return item.Spawn(position);
+        }
+
+        /// <inheritdoc/>
+        protected override void ShowSelectedMessage(Player player)
+        {
+            Handlers.GuardCommanderHandler.Instance.RunCoroutine(this.UpdateInterface(player));
+        }
+
+        /// <inheritdoc/>
+        protected override void OnDropping(DroppingItemEventArgs ev)
+        {
+            if (this.CurrentOwner is null)
+                return;
+            this.CurrentOwner = null;
+        }
+
+        /*/// <inheritdoc/>
+        public override bool OnDrop(Player player, Inventory item)
+        {
+            base.OnDrop(player, item);
+            if (CurrentOwner == null)
+                return true;
+            CurrentOwner.SetSessionVar(Main.SessionVarType.CI_GUARD_COMMANDER_KEYCARD_OWNER, false);
+            CurrentOwner = null;
+            return true;
+        }*/
+
+        /// <inheritdoc/>
+        protected override void OnWaitingForPlayers()
+        {
+            this.CurrentOwner = null;
+        }
+
+        private IEnumerator<float> UpdateInterface(Player player)
+        {
+            yield return Timing.WaitForSeconds(0.1f);
+
+            while (this.Check(player.CurrentItem))
+            {
+                if (MistakenCustomRoles.GUARD_COMMANDER.Get().Check(player) || player == this.CurrentOwner)
+                    player.SetGUI("GC_Keycard", PseudoGUIPosition.BOTTOM, "<color=yellow>Trzymasz</color> kartę <color=blue>Dowódcy Ochrony</color>");
+                else
+                    player.SetGUI("GC_Keycard", PseudoGUIPosition.BOTTOM, "<color=yellow>Trzymasz</color> kartę <color=blue>Dowódcy Ochrony</color>, ale chyba <color=yellow>nie</color> możesz jej używać");
+
+                yield return Timing.WaitForSeconds(1f);
+            }
+
+            player.SetGUI("GC_Keycard", PseudoGUIPosition.BOTTOM, null);
+        }
+    }
+}
